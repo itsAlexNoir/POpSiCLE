28201548ddbcc0dfe3d0addcfb506a0a043638d1
Alex <a.delacalle@qub.ac.uk>
2015-07-14 19:35:04 +0100

    added get_subset routine.

--- src/popsicle.f90
+++ src/popsicle.f90
@@ -2,9 +2,11 @@ MODULE POPSICLE
   
   ! Here the list of modules of the library
   USE constants
+  USE tools
   USE interp
   USE coords_transformation
   USE fourier
+  USE patchwork
   USE io
   
   IMPLICIT NONE
