6ba7e09334e87ba007d915a8189e3f2c64fc6fbe
Alejandro de la Calle <alejandrodelacallenegro@gmail.com>
2016-06-17 17:22:59 +0200

    Better input/output through the screen in calculator.f90 utilities.
    Former-commit-id: 788b2bd1dfdca921e82a6832f912468601c424c7

--- utility/sampling_calculator.f90
+++ utility/sampling_calculator.f90
@@ -43,10 +43,14 @@ PROGRAM sampling_calculator
   WRITE(*, *) '-------------------------'
   READ(*, '(1A80)') filename_sampling
   WRITE(*, *)
+  WRITE(*, *) 'Filename :',filename_sampling
+  WRITE(*, *)
   
   WRITE(*, *) 'Boundary radius (a.u.):'
   WRITE(*, *) '-----------------------'
   READ(*, *) radius_boundary
+  WRITE(*, *)
+  WRITE(*, '(A,F9.3)') 'Rb: ',radius_boundary
   WRITE(*, *)
 
   WRITE(*, *) 'Do you want to add time at the end of the evolution? (y or n)'
@@ -57,12 +61,24 @@ PROGRAM sampling_calculator
   aftertime_fs = 0.0_dp
   
   IF ((answer .EQ. 'Y') .OR. (answer .EQ. 'y')) THEN
+
+     WRITE(*, *)
+     WRITE(*, *) 'Yes'
+     WRITE(*, *)
      
      WRITE(*, *) 'Time added after the evolution (fs):'
      WRITE(*, *) '--------------------------------'
      READ(*, *) aftertime_fs
      WRITE(*, *)
-     
+     WRITE(*, '(A,F9.3)') 'Time after: ',aftertime_fs
+     WRITE(*, *)
+
+  ELSE
+
+     WRITE(*, *)
+     WRITE(*, *) 'No'
+     WRITE(*, *)
+
   ENDIF
   
   WRITE(*, *) 'Do you want to add the Coulomb explosion energy? (y or n)'
@@ -73,12 +89,24 @@ PROGRAM sampling_calculator
   coulomb_exp_energy = 0.0_dp
   
   IF ((answer .EQ. 'Y') .OR. (answer .EQ. 'y')) THEN
+
+     WRITE(*, *)
+     WRITE(*, *) 'Yes'
+     WRITE(*, *)
      
      WRITE(*, *) 'Coulomb explosion energy (a.u.):'
      WRITE(*, *) '--------------------------------'
      READ(*, *) coulomb_exp_energy
      WRITE(*, *)
-     
+     WRITE(*, '(A,F9.3)') 'Coulomb explosion energy: ',coulomb_exp_energy
+     WRITE(*, *)
+
+  ELSE
+
+     WRITE(*, *)
+     WRITE(*, *) 'No'
+     WRITE(*, *)
+
   ENDIF
   
   WRITE(*, *) 'Do you want to gauge transform from length to velocity (y or n)'
@@ -90,8 +118,18 @@ PROGRAM sampling_calculator
   
   IF ((answer .EQ. 'Y') .OR. (answer .EQ. 'y')) THEN
      
-     desired_gauge_trans = .TRUE.
+     WRITE(*, *)
+     WRITE(*, *) 'Yes'
+     WRITE(*, *)
      
+     desired_gauge_trans = .TRUE.
+
+  ELSE
+     
+     WRITE(*, *)
+     WRITE(*, *) 'No'
+     WRITE(*, *)
+
   ENDIF
   
   
@@ -104,19 +142,32 @@ PROGRAM sampling_calculator
   
   IF ((answer .EQ. 'Y') .OR. (answer .EQ. 'y')) THEN
 
+     WRITE(*, *)
+     WRITE(*, *) 'Yes'
+     WRITE(*, *)
+
      desired_wavevstime = .TRUE.
      
      WRITE(*, *) 'Name of the wave vs time file.  '
      WRITE(*, *) '--------------------------------'
      READ(*, *) filename_wavevstime
      WRITE(*, *)
-     
+     WRITE(*, *) 'Filename for wave vs. time: ',filename_wavevstime
+     WRITE(*, *)
+
+  ELSE
+
+     WRITE(*, *)
+     WRITE(*, *) 'No'
+     WRITE(*, *)
      
   ENDIF
   
   WRITE(*, *) 'Name of the PES file:'
   WRITE(*, *) '---------------------'
   READ(*,'(1A80)') filename_pes
+  WRITE(*, *)
+  WRITE(*, *) 'Filename for PES: ',filename_pes
   WRITE(*, *)
   
 
@@ -128,35 +179,59 @@ PROGRAM sampling_calculator
   desired_amplitude = .FALSE.
   
   IF ((answer .EQ. 'Y') .OR. (answer .EQ. 'y')) THEN
-     
+
+     WRITE(*, *)
+     WRITE(*, *) 'Yes'
+     WRITE(*, *)
+
      desired_amplitude = .TRUE.
 
      WRITE(*, *) 'Name of the spherical amplitude file:'
      WRITE(*, *) '-------------------------------------'
      READ(*,'(1A80)') filename_amplitude
      WRITE(*, *)
-   
+     WRITE(*, *) 'Filename for spherical amplitude: ',filename_amplitude
+     WRITE(*, *)
+
+  ELSE
+
+     WRITE(*, *)
+     WRITE(*, *) 'No'
+     WRITE(*, *)
+
   ENDIF
   
   WRITE(*, *) 'Do you want polar scattering amplitude? (y or n)'
   WRITE(*, *) '------------------------------------------------'
   READ(*, '(1A1)') answer
   WRITE(*, *)
-
+  
   desired_polar = .FALSE.
   
   IF ((answer .EQ. 'Y') .OR. (answer .EQ. 'y')) THEN
      
+     WRITE(*, *)
+     WRITE(*, *) 'Yes'
+     WRITE(*, *)
+     
      desired_polar = .TRUE.
-
+     
      WRITE(*, *) 'Name of the polar amplitude file:'
      WRITE(*, *) '---------------------------------'
      READ(*,'(1A80)') filename_polar
      WRITE(*, *)
-   
+     WRITE(*, *) 'Filename for polar amplitude: ',filename_polar
+     WRITE(*, *)
+     
+  ELSE
+     
+     WRITE(*, *)
+     WRITE(*, *) 'No'
+     WRITE(*, *)
+     
   ENDIF
   
-
+  
   CALL initialize_sampling_points(filename_sampling, radius_boundary, &
        numdetectorpts, numthetapts, numphipts, aftertime_fs, &
        desired_gauge_trans, coulomb_exp_energy, numwpts )
@@ -165,7 +240,7 @@ PROGRAM sampling_calculator
   ! Allocate momentum amplitude array
   ALLOCATE(pes(1:numwpts))
   ALLOCATE(pad(1:numthetapts,1:numphipts,1:numwpts))
-
+  
   ! The name is self-explanatory
   CALL calculate_sampling_pes(filename_sampling, pes, pad)
   
